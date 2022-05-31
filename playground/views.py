05ed03b28bdd10dd7c36d9db4b5fa07f31e55c98
SrijitaSarkar99 <srijitasarkar9912@gmail.com>
2022-05-31 10:25:13 +0530

    first commit

---
+++ playground/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request->response
+# request handler
+# action
+
+def say_hello(request):
+    # pull data from db, tranform,send emails
+    # return  HttpResponse('Hello World')
+    x = 1
+    y = 2
+    return render(request, 'hello.html', {'name':'Sabita'})
