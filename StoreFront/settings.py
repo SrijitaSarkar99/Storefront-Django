46881fa169ba38b187c37e85ac3bfc4d05a04913
SrijitaSarkar99 <srijitasarkar9912@gmail.com>
2022-06-05 13:38:20 +0530

    Created Customer DB

--- StoreFront/settings.py
+++ StoreFront/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'StoreFront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'StoreFront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Miraki@912'
     }
 }
 
